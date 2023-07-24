366cf6ad05b7109395e47f54c9622b617ce2643e
khadija <abakil.khadija25@gmail.com>
2023-07-24 07:24:11 -0700

    add functions : p_hex , p_octal and p_octal

--- main.h
+++ main.h
@@ -13,6 +13,10 @@ int p_string(va_list s);
 int p_int(va_list i);
 int p_dec(va_list d);
 int p_bin(va_list b);
+int p_unsig(va_list u);
+int p_octal(va_list o);
+int p_x(va_list x);
+int p_X(va_list X);
 /**
  * struct code_format - Struct format
  * @sp: Specifiers
