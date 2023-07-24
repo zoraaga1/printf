5ace8aa365e1a36ae176d219265890d7e12b25e1
zoraaga <fati.sousse12@gmail.com>
2023-07-24 12:45:00 -0700

    Fix betty style

--- p_S.c
+++ p_S.c
@@ -1,33 +1,34 @@
 #include "main.h"
 
 /**
- * p_S - print the input string with special formatting for non-printable characters
+ * p_S - print the input string
+ *	with special formatting
+ *	for non-printable characters
  * @s: the input string
  * Return: number of characters printed
  */
 int p_S(va_list s)
 {
-    char *str = va_arg(s, char *);
-    int s_printed = 0;
+	char *str = va_arg(s, char *);
+	int s_printed = 0;
 
-    if (str == NULL)
-        str = "(null)";
+	if (str == NULL)
+		str = "(null)";
 
-    while (*str)
-    {
-        if (*str < 32 || *str >= 127)
-        {
-            s_printed += _putchar('\\');
-            s_printed += _putchar('x');
-            s_printed += _putchar('0' + ((*str >> 4) & 0xF));
-            s_printed += _putchar('0' + (*str & 0xF));
-        }
-        else
-        {
-            s_printed += _putchar(*str);
-        }
-        str++;
-    }
-
-    return (s_printed);
+	while (*str)
+	{
+		if (*str < 32 || *str >= 127)
+		{
+			s_printed += _putchar('\\');
+			s_printed += _putchar('x');
+			s_printed += _putchar('0' + ((*str >> 4) & 0xF));
+			s_printed += _putchar('0' + (*str & 0xF));
+		}
+		else
+		{
+			s_printed += _putchar(*str);
+		}
+	str++;
+	}
+	return (s_printed);
 }
