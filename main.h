857f7a0c3ac0efc8c9664f87928d25401d5d0afb
khadija <abakil.khadija25@gmail.com>
2023-07-24 07:01:40 -0700

    add functions : p_hex , p_octal and p_unsig

--- main.h
+++ main.h
@@ -12,7 +12,10 @@ int p_char(va_list c);
 int p_string(va_list s);
 int p_int(va_list i);
 int p_dec(va_list d);
-
+int p_unsig(va_list u);
+int p_octal(va_list o);
+int p_x(va_list x);
+int p_X(va_list X);
 /**
  * struct code_format - Struct format
  * @sp: Specifiers
